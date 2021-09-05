17b33b4298b2adb44db06ab6d2769aceb298832d
Henry Wang <hwang94@hotmail.com>
2021-09-05 16:34:26 -0700

    2nd commit

--- Run.php
+++ Run.php
@@ -6,6 +6,6 @@ include('RoundResult.php');
 include('RaceResult.php');
 
 // run a race and print the results
- $test = new Race();
+ $test = new Race;
  $results = $test->runRace();
  print_r($results->getRoundResults());
